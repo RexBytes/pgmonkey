c2defad6b5945d090f217b97584c1d80813aea01
goodboy <goodboy@rexbytes.com>
2024-10-15 17:28:52 +0200

    minor changes connconfig alias added for filepath

--- src/cli/cli_pgconfig_subparser.py
+++ src/cli/cli_pgconfig_subparser.py
@@ -7,7 +7,6 @@ def cli_pgconfig_subparser(subparsers):
     pgconfig_manager = PGConfigManager()
     pgcodegen_manager = PGCodegenManager()
 
-
     pgconfig_parser = subparsers.add_parser('pgconfig', help='Manage database configurations')
     # Note, the following line is adding a subparser to this subparser which is allowed.
     pgconfig_subparsers = pgconfig_parser.add_subparsers(dest='pgconfig_command', help='pgconfig commands')
@@ -16,22 +15,25 @@ def cli_pgconfig_subparser(subparsers):
     create_parser = pgconfig_subparsers.add_parser('create', help='Create a new database configuration file.')
     create_parser.add_argument('--type', choices=['pg'], default='pg', required=False,
                                help='Database type for the configuration template. Default is "pg".')
-    create_parser.add_argument('--filepath', required=True,
+    # Adding alias: --connconfig for --filepath
+    create_parser.add_argument('--filepath', '--connconfig', required=True,
                                help='Path to where you want your configuration file created.')
     create_parser.set_defaults(func=pgconfig_create_handler, pgconfig_manager=pgconfig_manager)
 
     # The "test" subcommand
     test_parser = pgconfig_subparsers.add_parser('test',
                                                  help='Test the database connection using a configuration file.')
-    test_parser.add_argument('--filepath', required=True, help='Path to the configuration file you want to test.')
+    # Adding alias: --connconfig for --filepath
+    test_parser.add_argument('--filepath', '--connconfig', required=True,
+                             help='Path to the configuration file you want to test.')
     test_parser.set_defaults(func=pgconfig_test_handler, pgconfig_manager=pgconfig_manager)
 
     # The "generate-code" subcommand
     code_parser = pgconfig_subparsers.add_parser('generate-code', help='Generate Python code to connect using a configuration file.')
-    code_parser.add_argument('--filepath', required=True, help='Path to the configuration file you want to use.')
+    # Adding alias: --connconfig for --filepath
+    code_parser.add_argument('--filepath', '--connconfig', required=True,
+                             help='Path to the configuration file you want to use.')
     code_parser.set_defaults(func=pgconfig_generate_code_handler, pgcodegen_manager=pgcodegen_manager)
-
-
 
 
 def pgconfig_create_handler(args):
@@ -51,3 +53,4 @@ def pgconfig_test_handler(args):
 def pgconfig_generate_code_handler(args):
     pgcodegen_manager = args.pgcodegen_manager
     pgcodegen_manager.generate_connection_code(args.filepath)
+
